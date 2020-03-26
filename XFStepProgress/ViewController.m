a1559ca7f396c6d576c364f62591d58802bd5de8
乐易学 <18709863550@163.com>
2020-03-26 11:01:46 +0800

    修复bug

--- XFStepProgress/ViewController.m
+++ XFStepProgress/ViewController.m
@@ -21,22 +21,21 @@
     [super viewDidLoad];
     NSLog(@"这是一个测试用例");
     _stepView = [[XFStepView alloc]initWithFrame:CGRectMake(0, 50, self.view.frame.size.width, 60) Titles:[NSArray arrayWithObjects:@"第一步", @"第二步", @"第三步", nil]];
-    _stepView.stepIndex = 1;
+    _stepView.stepIndex = 3;
     [self.view addSubview:_stepView];
 }
 
-//- (IBAction)clickPer:(id)sender
-//{
-//
-//    int step = _stepView.stepIndex;
-//    [_stepView setStepIndex:step - 1 Animation:NO];
-//}
-//
-//- (IBAction)clickNext:(id)sender
-//{
-//    int step = _stepView.stepIndex;
-//    [_stepView setStepIndex:step + 1 Animation:YES];
-//}
+- (IBAction)clickPer:(id)sender
+{
 
+    int step = _stepView.stepIndex;
+    [_stepView setStepIndex:step - 1 Animation:YES];
+}
+
+- (IBAction)clickNext:(id)sender
+{
+    int step = _stepView.stepIndex;
+    [_stepView setStepIndex:step + 1 Animation:YES];
+}
 
 @end
